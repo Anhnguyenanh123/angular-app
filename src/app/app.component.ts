619fddd9b04f7c781377a43e6df2c2bc44f93fbe
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2024-08-14 12:08:09 +0700

    add some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
@@ -10,12 +10,12 @@ import { DUMMY_USERS } from './dummy-users';
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   users = DUMMY_USERS;
 
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
@@ -25,5 +25,4 @@ export class AppComponent {
     // this.selectedUser = this.users.find((user) => user.id === id)!;
     this.selectedUserId = id;
   }
-
 }
