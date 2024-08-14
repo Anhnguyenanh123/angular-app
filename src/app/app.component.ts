6144b49850f1bd80e6297c573aa25d1c5cced37b
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2024-08-14 12:42:27 +0700

    add some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './tasks/task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,42 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { title } from 'process';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Task 1',
+      summary: 'Task 1',
+      dueDate: '2022-01-01',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Task 1',
+      summary: 'Task 1',
+      dueDate: '2022-01-01',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Task 1',
+      summary: 'Task 1',
+      dueDate: '2022-01-01',
+    },
+  ];
+
+  get selectedUserTask() {
+    return this.tasks.filter((task) => task.userId === this.userId)!;
+  }
 }
